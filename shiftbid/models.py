3d6fb5f7f3cb007f317b7a39f29ddaf7f8849a58
tanley5 <tanleybench@gmail.com>
2022-05-06 08:33:20 -0600

    Added Shiftbid Model

--- shiftbid/models.py
+++ shiftbid/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Shfitbid(models.Model):
+    SHIFTSTATUS = (
+        ('c','created'),
+        ('s','started'),
+        ('e','stopped')
+    )
+    
+    shiftbid_name = models.CharField(max_length=30)
+    date_created = models.DateTimeField(auto_now_add=True)
+    shift_status = models.CharField(max_length=1,choices=SHIFTSTATUS, default='c')
+
+    def __str__(self):
+        return f"Shiftbid: {self.shiftbid_name}"
