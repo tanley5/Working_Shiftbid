2022db4f56b3ad15d9234bc89e04098b31f6a54c
tanley5 <tanleybench@gmail.com>
2022-05-16 15:51:58 -0600

    Added Scheduler function

--- shiftbid/management/commands/shiftbid_startup_custom_command.py
+++ shiftbid/management/commands/shiftbid_startup_custom_command.py
@@ -1,16 +1,22 @@
 from django.core.management.base import BaseCommand, CommandError
 import schedule
-
+import time
 from shiftbid.models import Shiftbid, Seniority, Shift
+
+def scheduled_task():
+    print("Started At Startup")
+    sb = Shiftbid.objects.all()
+    for i in sb:
+        print(i)
 
 class Command(BaseCommand):
     help = 'My custom startup command'
 
     def handle(self, *args, **kwargs):
         try:
-            print("Started At Startup")
-            sb = Shiftbid.objects.all()
-            for i in sb:
-                print(i)
+            schedule.every(1).minutes.do(scheduled_task)
+            while True:
+                schedule.run_pending()
+                time.sleep(1)
         except:
             raise CommandError('Initalization failed.')
